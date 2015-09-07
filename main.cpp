faa7387e88fa8af757e8e17a476a9ae55745ba3e
Dmytro Haponov <dmytro.haponov@gmail.com>
2015-09-07 19:20:41 +0300

    minor update

--- main.cpp
+++ main.cpp
@@ -4,14 +4,9 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-
-
     QQuickView view;
     view.setResizeMode(QQuickView::SizeRootObjectToView);
     view.setSource(QUrl("qrc:///Screen1.qml"));
     view.show();
     return app.exec();
 }
-
-
-
